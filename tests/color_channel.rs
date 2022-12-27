d5287ac095f22f3b11a7fc54d68293d0ea9d10ca
林佳涛 <lne128gene980@163.com>
2022-12-27 14:56:09 +0800

    tests: add color_channel

---
+++ tests/color_channel.rs
@@ -0,0 +1,19 @@
+use color_art::Color;
+use std::str::FromStr;
+
+#[test]
+fn test_color_channel() {
+    let color = Color::from_str("#abcdef").unwrap();
+
+    assert_eq!(color.red(), 171);
+    assert_eq!(color.green(), 205);
+    assert_eq!(color.blue(), 239);
+    assert_eq!(color.alpha(), 1.0);
+
+    assert_eq!(color.hue(), 210.0);
+    assert_eq!(color.saturation(), 0.68);
+    assert_eq!(color.lightness(), 0.8);
+
+    assert_eq!(color.luma(), 0.59);
+    assert_eq!(color.luminance(), 0.79);
+}
