16c6cf4dcad70738e2b49b650a4652c9b57e4eb3
林佳涛 <lne128gene980@163.com>
2023-01-06 15:11:02 +0800

    refactor: distance

--- src/color_calc/distance.rs
+++ src/color_calc/distance.rs
@@ -1,17 +1,17 @@
-use crate::Color;
+use crate::{Color, ColorSpace};
 
 /// Computes the [Euclidean distance](https://en.wikipedia.org/wiki/Euclidean_distance#Three_dimensions) between two colors.
 pub fn distance(color1: Color, color2: Color) -> f64 {
-    let rgba1 = color1.space(crate::ColorSpace::RGBA).unwrap();
-    let rgba2 = color2.space(crate::ColorSpace::RGBA).unwrap();
+    let vec1 = color1.space(ColorSpace::RGBA).unwrap();
+    let vec2 = color2.space(ColorSpace::RGBA).unwrap();
 
     let mut d = 0.0;
 
-    for i in 0..rgba1.len() {
-        d += (rgba1[i] - rgba2[i]).powf(2.0)
-    }
+    vec1.iter().zip(vec2.iter()).for_each(|(a, b)| {
+        d += (a - b).powf(2.0);
+    });
 
-    d.powf(0.5)
+    d.sqrt()
 }
 
 #[cfg(test)]
