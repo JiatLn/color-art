5a286aaffe297a59e10a83df408700e354c50261
cx33 <lne128gene980@163.com>
2023-04-11 21:39:59 +0800

    feat: create color from cmyk values

--- src/color/from_space.rs
+++ src/color/from_space.rs
@@ -5,6 +5,7 @@ impl Color {
     /// Create a color from RGB values.
     ///
     /// # Example
+    ///
     /// ```
     /// use color_art::Color;
     ///
@@ -18,6 +19,7 @@ impl Color {
     /// Create a color from RGBA values.
     ///
     /// # Example
+    ///
     /// ```
     /// use color_art::Color;
     /// let color = Color::from_rgba(255.0, 51.0, 153.0, 0.5).unwrap();
@@ -30,6 +32,7 @@ impl Color {
     /// Create a color from HSL values.
     ///
     /// # Example
+    ///
     /// ```rust
     /// use color_art::Color;
     ///
@@ -44,6 +47,7 @@ impl Color {
     /// Create a color from HSV values.
     ///
     /// # Example
+    ///
     /// ```
     /// use color_art::Color;
     ///
@@ -53,6 +57,21 @@ impl Color {
     pub fn from_hsv(h: f64, s: f64, v: f64) -> Result<Color> {
         ColorSpace::HSV.valid(&vec![h, s, v])?;
         let (r, g, b) = conversion::hsv::hsv2rgb((h, s, v));
+        Ok(Color::new(r, g, b, 1.0))
+    }
+    /// Create a color from CMYK values.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use color_art::Color;
+    ///
+    /// let color = Color::from_cmyk(0.0, 0.8, 0.4, 0.0).unwrap();
+    /// assert_eq!(color.hex(), "#ff3399");
+    /// ```
+    pub fn from_cmyk(c: f64, m: f64, y: f64, k: f64) -> Result<Color> {
+        ColorSpace::CMYK.valid(&vec![c, m, y, k])?;
+        let (r, g, b) = conversion::cmyk::cmyk2rgb((c, m, y, k));
         Ok(Color::new(r, g, b, 1.0))
     }
 }
@@ -83,5 +102,17 @@ mod tests {
     fn test_color_from_hsv() {
         let color = Color::from_hsv(38.82, 1.0, 1.0).unwrap();
         assert_eq!(color.hex(), "#ffa500");
+    }
+
+    #[test]
+    fn test_color_from_cmyk() {
+        let color = Color::from_cmyk(0.0, 0.8, 0.4, 0.0).unwrap();
+        assert_eq!(color.hex(), "#ff3399");
+
+        let color = Color::from_cmyk(0.2, 0.8, 0.0, 0.0).unwrap();
+        assert_eq!(color.hex(), "#cc33ff");
+
+        let color = Color::from_cmyk(2.0, 0.0, 0.0, 1.0);
+        assert!(color.is_err());
     }
 }
