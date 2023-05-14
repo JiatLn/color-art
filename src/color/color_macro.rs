95d1cc741fb39d9874da8edcc66b22799c4d6287
cx33 <lne128gene980@163.com>
2023-05-14 21:34:53 +0800

    update: improve `color!` marco

--- src/color/color_macro.rs
+++ src/color/color_macro.rs
@@ -15,13 +15,25 @@ macro_rules! color_args {
 macro_rules! color {
     ($color_space:ident, $($args:tt)*) => {
         {
+            let args = $crate::color_args!($($args)*);
             match stringify!($color_space).into() {
                 $crate::ColorSpace::RGB => {
-                    let args = $crate::color_args!($($args)*);
-                    let r = args[0];
-                    let g = args[1];
-                    let b = args[2];
+                    let r = args[0] as u8;
+                    let g = args[1] as u8;
+                    let b = args[2] as u8;
                     $crate::Color::from_rgb(r, g, b).unwrap()
+                }
+                $crate::ColorSpace::HSL => {
+                    let h = args[0] as f64;
+                    let s = args[1] as f64;
+                    let l = args[2] as f64;
+                    $crate::Color::from_hsl(h, s, l).unwrap()
+                },
+                $crate::ColorSpace::HSV => {
+                    let h = args[0] as f64;
+                    let s = args[1] as f64;
+                    let v = args[2] as f64;
+                    $crate::Color::from_hsv(h, s, v).unwrap()
                 }
                 _ => todo!("Add more color spaces to color! macro"),
             }
@@ -46,5 +58,11 @@ mod tests {
     fn test_color_macro() {
         let color = color!(rgb, 255, 255, 0);
         assert_eq!(color.rgb(), "rgb(255, 255, 0)");
+
+        let color = color!(hsl, 60.0, 1.0, 0.5);
+        assert_eq!(color.hsl(), "hsl(60, 100%, 50%)");
+
+        let color = color!(hsv, 60.0, 1.0, 1.0);
+        assert_eq!(color.hsv(), "hsv(60, 100%, 100%)");
     }
 }
