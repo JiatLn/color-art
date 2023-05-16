3424cb173f00620161e0876fd085d8f8ba0a0fc6
林佳涛 <lne128gene980@163.com>
2023-05-16 09:27:46 +0800

    feat: color! support define rgb(a) color space

--- src/color/color_macro.rs
+++ src/color/color_macro.rs
@@ -22,6 +22,12 @@ macro_rules! color {
             $crate::Color::from_hex(&hex).unwrap()
         }
     };
+    (rgb($r:expr, $g:expr, $b:expr)) => {
+        $crate::Color::from_rgb($r, $g, $b).unwrap()
+    };
+    (rgba($r:expr, $g:expr, $b:expr, $a:expr)) => {
+        $crate::Color::from_rgba($r, $g, $b, $a).unwrap()
+    };
     (
         $color_space:ident,
         $($args:tt)*
@@ -30,9 +36,9 @@ macro_rules! color {
             let args = $crate::color_args!($($args)*);
             match stringify!($color_space).into() {
                 $crate::ColorSpace::RGB => {
-                    let r = args[0] as u8;
-                    let g = args[1] as u8;
-                    let b = args[2] as u8;
+                    let r = args[0] as f64;
+                    let g = args[1] as f64;
+                    let b = args[2] as f64;
                     $crate::Color::from_rgb(r, g, b).unwrap()
                 }
                 $crate::ColorSpace::HSL => {
@@ -68,6 +74,12 @@ mod tests {
     fn test_color_macro() {
         let color = color!(rgb, 255, 255, 0);
         assert_eq!(color.rgb(), "rgb(255, 255, 0)");
+
+        let color = color!(rgb(255, 255, 0));
+        assert_eq!(color.rgb(), "rgb(255, 255, 0)");
+
+        let color = color!(rgba(255, 255, 0, 0.5));
+        assert_eq!(color.rgba(), "rgba(255, 255, 0, 0.5)");
 
         let color = color!(hsl, 60.0, 1.0, 0.5);
         assert_eq!(color.hsl(), "hsl(60, 100%, 50%)");
---
+++ tests/color_macros.rs
@@ -0,0 +1,16 @@
+use color_art::color;
+
+#[test]
+fn test_color_marcos() {
+    let color = color!(rgb, 255, 255, 0);
+    assert_eq!(color.rgb(), "rgb(255, 255, 0)");
+
+    let color = color!(#1890ff);
+    assert_eq!(color.hex(), "#1890ff");
+
+    let color = color!(rgb(255, 255, 0));
+    assert_eq!(color.rgb(), "rgb(255, 255, 0)");
+
+    let color = color!(rgba(255, 255, 0, 0.5));
+    assert_eq!(color.rgba(), "rgba(255, 255, 0, 0.5)");
+}
