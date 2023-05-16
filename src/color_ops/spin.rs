acbb8ead02bf3c845303e547a89cfcc3b1497c57
林佳涛 <lne128gene980@163.com>
2023-05-16 16:38:26 +0800

    feat(Color): add `complement`

--- src/color_ops/spin.rs
+++ src/color_ops/spin.rs
@@ -29,6 +29,10 @@ impl Color {
         let h = (360.0 + h + angle) % 360.0;
         Color::from_hsl(h, s, l).unwrap()
     }
+    /// Returns the [complement](https://en.wikipedia.org/wiki/Complementary_colors) of color.
+    pub fn complement(&self) -> Self {
+        self.spin(180.0)
+    }
 }
 
 #[cfg(test)]
@@ -45,5 +49,20 @@ mod tests {
         let color = Color::from_str("hsl(10, 90%, 50%)").unwrap();
         let color = color.spin(-30.0);
         assert_eq!(color.hsl(), "hsl(340, 90%, 50%)");
+    }
+
+    #[test]
+    fn test_color_complement() {
+        let color = color!(#6b717f);
+        let color = color.complement();
+        assert_eq!(color.hex(), "#7f796b");
+
+        let color = color!(#d2e1dd);
+        let color = color.complement();
+        assert_eq!(color.hex(), "#e1d2d6");
+
+        let color = color!(#036);
+        let color = color.complement();
+        assert_eq!(color.hex(), "#630");
     }
 }
